6e6b22b2fdfa9e674462453736f9f6505f4ef579
Lucien Shaw <myxlc55@outlook.com>
2024-10-24 12:27:50 +0800

    fix: remove console output

--- configwebui/static/js/index.js
+++ configwebui/static/js/index.js
@@ -22,7 +22,6 @@ function clearFlashMessage() {
 }
 
 function changeCheckboxStyle() {
-    console.log('Changing checkbox style');
     const container = document.querySelector('#editor-container');
     const checkboxes = container.querySelectorAll('input[type="checkbox"]');
 
